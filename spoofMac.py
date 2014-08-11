1aae0c566ddc40f0be9b6862dcb986492b9e1067
felix1429 <thennig314@gmail.com>
2014-08-11 00:58:17 -0500

    add IP address and DNS server resestting

--- spoofMac.py
+++ spoofMac.py
@@ -1,6 +1,9 @@
-import winreg, random, sys, os, subprocess, multiprocessing, time
+import winreg, random, sys, os, subprocess, multiprocessing, time, pyperclip, ipaddress
 
 results = [True]
+toggleList = ["disable","enable"]
+
+
 def writeReg(mac):
     try:
         aReg = winreg.ConnectRegistry(None,winreg.HKEY_LOCAL_MACHINE)
@@ -12,12 +15,14 @@ def writeReg(mac):
         print("Registry write failed")
         return False
 
+
 def generateMac():
     address = [0x02, 0xDB, 0x30,
                random.randint(0x00, 0x7f),
                random.randint(0x00, 0x7f),
                random.randint(0x00, 0x7f)]
     return "".join(map(lambda x: '%02x' %x, address))
+
 
 def userInput():
     var = str(input("Would you like to continue and reset the network? (n to postpone\n) "))
@@ -27,8 +32,9 @@ def userInput():
     else:
         return True
 
+
 def countdown():
-    for x in range(15,0,-1):
+    for x in range(10,0,-1):
         if(x != 1):
             endStr = ", "
         else:
@@ -37,14 +43,45 @@ def countdown():
         sys.stdout.flush()
         time.sleep(1)
 
+
 def initialize(fd):
     sys.stdin = os.fdopen(fd)
 
+
 def getResults(boolean):
     results[0] = boolean
+
+
+def copyToClipboard(macString):
+    pyperclip.copy(macString)
+
+
+def toggleNetworkCard(toggleList):
+    for toggle in toggleList:
+        subprocess.call("wmic path win32_networkadapter where index=2 "
+                                + "call " + toggle)
+        time.sleep(1)
+
+    
+def setIP():
+    subprocess.call("netsh interface ip set address name=Wi-Fi "
+                + "source=static addr=192.168.2.3 "
+                + "mask=255.255.255.0 gateway=192.168.2.1 1")
+    print("IP set")
+
+
+def setDNS():
+    subprocess.call("netsh interface ip set dns name=Wi-Fi "
+                    + "source=static addr=8.8.8.8")
+    time.sleep(1)
+    subprocess.call("netsh interface ip add dns name=Wi-Fi "
+                    + "addr=8.8.4.4 index=2")
+    print("DNS set")
+    
     
 if __name__ == "__main__":
-    writeReg(generateMac())
+    newAddress = generateMac()
+    writeReg(newAddress)
 
     fn = [sys.stdin.fileno()] 
     p = multiprocessing.Pool(initializer = initialize, initargs = fn)
@@ -55,11 +92,14 @@ if __name__ == "__main__":
         p.apply_async(userInput, callback = getResults)
         q.apply(countdown)
         if(results[0]):
-            subprocess.call("wmic path win32_networkadapter where index=2 call disable")
-            subprocess.call("wmic path win32_networkadapter where index=2 call enable")
+            toggleNetworkCard(toggleList)
             go = False
         else:
             print("Postponing for 15 minutes")
             results[0] = True
             time.sleep(900)
-            
+
+    copyToClipboard(newAddress)
+    setIP()    
+    setDNS()
+
