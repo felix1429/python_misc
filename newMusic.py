c7e8d75a63ec972acd8cc2185f46ffbf3234e147
felix1429 <thennig314@gmail.com>
2015-05-20 00:31:42 -0700

    remember last artist entered and add optional cmd line argument to retrieve

--- newMusic.py
+++ newMusic.py
@@ -2,37 +2,51 @@
 #creates an album and an artist folder in music and album art directory of both
 #internal and external hdd
 
-import os
+import os, argparse
 
-def getArtist():
+def getArtist(args):
+    if args.a:
+        return getLastArtist()
     while True:
-        artist_name = str(input("Input the name of the artist: "))      
+        artistName = str(input("Input the name of the artist: "))      
         try:
-            if "/" in artist_name:
+            if "/" in artistName:
                 raise OSError()
-            os.makedirs("C://{0}".format(artist_name))
-            os.removedirs("C://{0}".format(artist_name))
-            return artist_name
+            os.makedirs("C://{0}".format(artistName))
+            os.removedirs("C://{0}".format(artistName))
+            writeFile(artistName)
+            return artistName
         except OSError:
             print("Invalid character for file name, please "
                   + "input a different artist name")
 
-
 def getAlbum():
     while True:
-        album_name = str(input("Input the name of the album: "))
+        albumName = str(input("Input the name of the album: "))
         try:
-            if "/" in album_name:
+            if "/" in albumName:
                 raise OSError()
-            os.makedirs("C://{0}".format(album_name))
-            os.removedirs("C://{0}".format(album_name))
-            return album_name
+            os.makedirs("C://{0}".format(albumName))
+            os.removedirs("C://{0}".format(albumName))
+            return albumName
         except OSError:
             print("Invalid character for file name, please "
                   + "input a different album name")
 
+def getLastArtist():
+    with open("artist.file", "r") as file:
+        return file.read()
+
+def writeFile(artist):
+    with open("artist.file", "w") as file:
+        file.write(artist)
+                   
 if __name__ == "__main__":
-    artist_name = getArtist()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", help = "set the artist as the last artist entered",
+                        action = "store_true")
+    args = parser.parse_args() 
+    artist_name = getArtist(args)
     album_name = getAlbum()
     w = "H://Music/Music/{0}/{1}".format(artist_name, album_name)
     x = "H://Music/Album Art/{0}/{1}".format(artist_name, album_name)
